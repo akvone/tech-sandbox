d55b5935fe52729c2c3a3ad6af8cb1b68a29eb9b
Kirill Vialkov <akvone@mail.ru>
2021-02-26 12:52:49 +0300

    Refactor property files

--- spring-only/src/main/java/com/akvone/properties/MainServicesProperties.kt
+++ spring-only/src/main/java/com/akvone/properties/MainServicesProperties.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import java.time.Duration
 
-@ConfigurationProperties("main.services")
+@ConfigurationProperties("modules.properties")
 @ConstructorBinding
 data class MainServicesProperties(
 
