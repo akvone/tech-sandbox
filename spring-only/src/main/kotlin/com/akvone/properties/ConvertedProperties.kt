3dcf00084fa1bd8b5a5f47e5b49b6d627a1a948e
Kirill Vialkov <akvone@mail.ru>
2021-03-15 22:22:05 +0300

    Enhance properties section

--- spring-only/src/main/kotlin/com/akvone/properties/ConvertedProperties.kt
+++ spring-only/src/main/kotlin/com/akvone/properties/ConvertedProperties.kt
@@ -2,6 +2,7 @@ package com.akvone.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.util.unit.DataSize
 import java.time.Duration
 import java.time.Period
 
@@ -17,10 +18,15 @@ data class ConvertedProperties(
     /**
      * New timeout description
      */
-    val newDefaultTimeout: Duration,
+    val duration: Duration,
 
     /**
-     * Period kdoc
+     * Period kDoc
      */
-    val period: Period
+    val period: Period,
+
+    /**
+     * DatSize kDoc
+     */
+    val dataSize: DataSize
 )
--- spring-only/src/main/kotlin/com/akvone/properties/PropertiesRunner.kt
+++ spring-only/src/main/kotlin/com/akvone/properties/PropertiesRunner.kt
@@ -2,22 +2,43 @@ package com.akvone.properties
 
 import com.akvone.lifecycle.MainEventListener
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Component
+import org.springframework.util.unit.DataSize
+import java.time.Duration
+import java.time.Period
 
 @EnableConfigurationProperties(ConvertedProperties::class)
 @Component
 class PropertiesRunner(
-    private val convertedProperties: ConvertedProperties,
+    val convertedProperties: ConvertedProperties,
+    @Value("\${modules.properties.defaultTimeout}") val defaultTimeout: Long,
+    @Value("\${modules.properties.duration}") val duration: Duration,
+    @Value("\${modules.properties.period}") val period: Period,
+    @Value("\${modules.properties.dataSize}") val dataSize: DataSize,
 ) : ApplicationRunner {
 
     private val log = LoggerFactory.getLogger(MainEventListener::class.java)
 
     override fun run(args: ApplicationArguments?) {
-        log.info("From Long: ${convertedProperties.defaultTimeout}")
-        log.info("From Duration: ${convertedProperties.newDefaultTimeout.toMillis()}")
-        log.info("From Period: ${convertedProperties.period.toTotalMonths()}")
+        log.info(
+            """From @ConfigurationProperties:
+             |${convertedProperties.defaultTimeout}
+             |${convertedProperties.duration}
+             |${convertedProperties.period}
+             |${convertedProperties.dataSize}
+             """.trimMargin()
+        )
+        log.info(
+            """From @Value:
+                |$defaultTimeout
+                |$duration
+                |$period
+                |$dataSize
+            """.trimMargin()
+        )
     }
 }
