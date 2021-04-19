c0bcc5804919a78eea3108c4d045ee2e3534faab
Kirill Vialkov <akvone@mail.ru>
2021-04-19 17:09:51 +0300

    Rename module spring-only -> spring-boot

--- scripts/compare-startup-time.ps1
+++ scripts/compare-startup-time.ps1
@@ -1,7 +1,7 @@
 # This script compares time for .jar startup and unpacked .jar startup
 # IMPORTANT: Before execution add `.close()` to finish SpringBoot process right after startup
 Set-Variable -Name "initialFolder" -Value "$PWD"
-Set-Variable -Name "moduleName" -Value "spring-only"
+Set-Variable -Name "moduleName" -Value "spring-boot"
 Set-Variable -Name "projectFolder" -Value "../$moduleName"
 Set-Variable -Name "extractedJarFolder" -Value "extracted"
 # ...
@@ -14,7 +14,7 @@ jar xf ../$moduleName-1.0-SNAPSHOT.jar
 cd ..
 # Measure time
 echo "Starting .jar version"
-Measure-Command {java -jar .\spring-only-1.0-SNAPSHOT.jar}
+Measure-Command {java -jar .\spring-boot-1.0-SNAPSHOT.jar}
 echo "Starting unpacked .jar version"
 cd $extractedJarFolder
 Measure-Command {java org.springframework.boot.loader.JarLauncher}
