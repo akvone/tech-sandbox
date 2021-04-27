0df7254bd7514c776e32dab482098bc9c831a94c
Kirill Vialkov <akvone@mail.ru>
2021-04-27 17:03:25 +0300

    Add a test

---
+++ spring-boot/src/test/kotlin/com/akvone/SuccessfulStartupTest.kt
@@ -0,0 +1,13 @@
+package com.akvone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SuccessfulStartupTest {
+
+    @Test
+    fun test(){
+        // no-op
+    }
+}
