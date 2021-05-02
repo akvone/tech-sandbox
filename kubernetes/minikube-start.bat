a1290df2ae27d9fbb3e12952f708738ae0e29ae7
Kirill Vialkov <akvone@mail.ru>
2021-05-02 21:38:20 +0300

    Add initial files for kubernetes/docker

---
+++ kubernetes/minikube-start.bat
@@ -0,0 +1,3 @@
+:: Read https://minikube.sigs.k8s.io/docs/start/
+minikube start
+minikube dashboard
---
+++ spring-boot/deployment/tag-and-push.bat
@@ -0,0 +1,2 @@
+docker tag spring-boot:1.0-SNAPSHOT akvone/sandbox-spring-boot:1.0-SNAPSHOT
+docker push akvone/sandbox-spring-boot:1.0-SNAPSHOT
