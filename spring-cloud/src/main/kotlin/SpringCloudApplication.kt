fff984301f073677668be0d26a2281d7de08f5a1
Kirill Vialkov <akvone@mail.ru>
2021-05-02 22:31:16 +0300

    Add spring-cloud module and spring-cloud-parents

---
+++ spring-cloud/src/main/kotlin/SpringCloudApplication.kt
@@ -0,0 +1,11 @@
+package com.akvone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringCloudApplication
+
+fun main() {
+    runApplication<SpringCloudApplication>()
+}
