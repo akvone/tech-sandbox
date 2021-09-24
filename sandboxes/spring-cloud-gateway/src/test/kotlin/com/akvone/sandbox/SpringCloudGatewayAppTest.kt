7a8fd299a60758001040ac60be94864d5bbc597a
Kirill Vialkov <akvone@mail.ru>
2021-09-24 21:11:11 +0300

    Add spring-cloud-gateway integration test

---
+++ sandboxes/spring-cloud-gateway/src/test/kotlin/com/akvone/sandbox/SpringCloudGatewayAppTest.kt
@@ -0,0 +1,11 @@
+package com.akvone.sandbox.com.akvone.sandbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+internal class SpringCloudGatewayAppTest {
+    @Test
+    fun startupTest() {
+    }
+}
