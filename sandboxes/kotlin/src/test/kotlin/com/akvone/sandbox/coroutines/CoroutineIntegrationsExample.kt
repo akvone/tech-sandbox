2c4a9adec07494f70c6369bf0a02f4bb06269c85
Kirill Vialkov <akvone@mail.ru>
2022-09-15 21:27:54 +0300

    Refactor :kotlin

--- sandboxes/kotlin/src/test/kotlin/com/akvone/sandbox/coroutines/CoroutineIntegrationsExample.kt
+++ sandboxes/kotlin/src/test/kotlin/com/akvone/sandbox/coroutines/CoroutineIntegrationsExample.kt
@@ -4,9 +4,9 @@ import kotlinx.coroutines.future.await
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 
-class CoroutineComponent {
+class CoroutineIntegrationsExample {
 
-    suspend fun supplyAsync(value: String): String {
+    suspend fun `CompletableFuture integration`(value: String): String {
         Executors.newWorkStealingPool()
         val completableFuture: CompletableFuture<String> = CompletableFuture.supplyAsync { value }
         return completableFuture.await()
